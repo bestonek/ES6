9301f485d8d1d558a6319a48cda52cabeba1e9b7
Dxl <1071042788@qq.com>
2017-06-07 17:50:25 +0800

    123

---
+++ app/js/class/lesson01.js
@@ -0,0 +1,15 @@
+{
+	let a = 'Mr.Jin';
+	let b = '24';
+	let m = `你好！我是${a},今年${b}`;
+	console.log(m)
+}
+
+{
+	let a = 'abc';
+	console.log(a.repeat(3))
+}
+
+{
+	console.log('12'.padStart(13, '0'))
+}
